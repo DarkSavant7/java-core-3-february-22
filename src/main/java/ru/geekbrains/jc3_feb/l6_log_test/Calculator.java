ce8f920f8fa005985a073a9d2a1bc0406dcbda59
Alex Grigorev <persian89g@gmail.com>
2022-02-28 22:34:57 +0300

    log test

---
+++ src/main/java/ru/geekbrains/jc3_feb/l6_log_test/Calculator.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.jc3_feb.l6_log_test;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+}
